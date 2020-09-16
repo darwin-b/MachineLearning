6ad0b644f77e23e3a7b265c94e8187087668e0de
darwin_b <darwin.bollepalli@gmail.com>
2020-09-17 04:38:57 +0530

    Final Version -v1 Can be improved to run by taking data file names as arguments from command line

--- DecisionTrees/VarianceImpurityDTree.py
+++ DecisionTrees/VarianceImpurityDTree.py
@@ -41,6 +41,8 @@ def varianceImpurity(data,target):
 
     return vi
 
+
+
 def viGain(data,feature, target):
 
     viGain =0
@@ -68,6 +70,7 @@ def viGain(data,feature, target):
         # Determine individual featurevalue entropy
         featureVi = varianceImpurity(dataPartition,target)
         viGain = viGain + partitionProbabilty*featureVi
+        # print("VI Gain :",viGain)
 
         # print("Feature Entropy - ",featureValue,featureEntropy)
 
@@ -102,7 +105,7 @@ def viDTree(data,depth,target):
 
         gain,split = viGain(data,feature,target)
         featureViGain = datasetVi - gain
-        # print("Feature :",feature," ViGain :",featureViGain)
+        # print("Feature :",feature," ViGain :",featureViGain," Gain: ",gain)
 
         # Choosing split attribute
         if featureViGain>highestViGain:
@@ -169,17 +172,18 @@ def accuracyMatrix(x_val,y_val,model):
     # fileAccuracy[file]=count/fileSize
 
     return accuracy,y_pred,truePos
-    print("Accuracy : ",fileAccuracy[file]," Runtime : ",fileRunTime[file])
+    # print("Accuracy : ",fileAccuracy[file]," Runtime : ",fileRunTime[file])
 
 
 testDataFiles  = ["test_c300_d100","test_c300_d1000","test_c300_d5000","test_c500_d100","test_c500_d1000","test_c500_d5000","test_c1000_d100","test_c1000_d1000","test_c1000_d5000","test_c1500_d100","test_c1500_d1000","test_c1500_d5000","test_c1800_d100","test_c1800_d1000","test_c1800_d5000"]
 validDataFiles = ["valid_c300_d100","valid_c300_d1000","valid_c300_d5000","valid_c500_d100","valid_c500_d1000","valid_c500_d5000","valid_c1000_d100","valid_c1000_d1000","valid_c1000_d5000","valid_c1500_d100","valid_c1500_d1000","valid_c1500_d5000","valid_c1800_d100","valid_c1800_d1000","valid_c1800_d5000"]
 trainDataFiles = ["train_c300_d100","train_c300_d1000","train_c300_d5000","train_c500_d100","train_c500_d1000","train_c500_d5000","train_c1000_d100","train_c1000_d1000","train_c1000_d5000","train_c1500_d100","train_c1500_d1000","train_c1500_d5000","train_c1800_d100","train_c1800_d1000","train_c1800_d5000"]
 
-testDataFiles  = [testDataFiles[6]]
-validDataFiles = [validDataFiles[6]]
-trainDataFiles = [trainDataFiles[6]]
 
+# testDataFiles  = [testDataFiles[12]]
+# validDataFiles = [validDataFiles[12]]
+# trainDataFiles = [trainDataFiles[12]]
+#
 
 # Count number of files
 files = testDataFiles.__len__()
@@ -193,7 +197,7 @@ fileRunTime={}
 
 # temp = dTree(data2,0,"Play")
 
-# DTree resultsMatrix with Entropy as impurity heuristic
+# DTree resultsMatrix with Variance as impurity heuristic
 for file in range(0,files):
 
     start = time.time()
@@ -212,21 +216,44 @@ for file in range(0,files):
     fileAccuracy[file], filePred[file], fileTruePos[file] = accuracyMatrix(x_test,y_test,fileModel[file])
     print(testDataFiles[file]," ==> ","Accuracy : ",fileAccuracy[file]," Runtime : ",fileRunTime[file])
 
-
-
-# DTree resultsMatrix with Entropy as impurity heuristic and depth based pruning
+#
+# files = testDataFiles.__len__()
+# DTree resultsMatrix with Variance as impurity heuristic and depth based pruning
 print("")
 print("")
-print("Depth based pruning with Entropy as heuristic")
-for file in validDataFiles:
-    x_valid,y_valid,dataValid = data(file)
-    print(file)
+print("Depth based pruning with Variance as heuristic")
+for file in range(0,files):
+    # not yet working/ using valid data files : to be used in reduced error pruning
+    x_valid,y_valid,dataValid = data(validDataFiles[file])
+    x_test,y_test,dataTest = data(testDataFiles[file])
+    print(testDataFiles[file])
+
+    # Depth --> 20,15,10,5
     for depth in range(20,0,-5):
-        tuningModel=copy.deepcopy(fileModel[validDataFiles.index(file)])
+        tuningModel=copy.deepcopy(fileModel[file])
         tuningModel.depthPrune(depth)
 
         acc,pre,pos=accuracyMatrix(x_test,y_test,tuningModel)
         print("depth :",depth,"accuracy :",acc)
 
 
+f = open( 'Models-VarGain.txt', 'w' )
+f.write( 'dict = ' + repr(fileModel) + '\n' )
+f.close()
+
+f = open( 'Pred-VarGain.txt', 'w' )
+f.write( 'dict = ' + repr(filePred) + '\n' )
+f.close()
+
+f = open( 'Accuracy-VarGain.txt', 'w' )
+f.write( 'dict = ' + repr(fileAccuracy) + '\n' )
+f.close()
+
+f = open( 'Runtimes-VarGain.txt', 'w' )
+f.write( 'dict = ' + repr(fileRunTime) + '\n' )
+f.close()
+
+f = open( 'TruePositives-VarGain.txt', 'w' )
+f.write( 'dict = ' + repr(fileTruePos) + '\n' )
+f.close()
 
