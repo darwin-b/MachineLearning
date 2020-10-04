fde8549cc3ffb14574790e1b1eaf87919221809f
darwin_b <darwin.bollepalli@gmail.com>
2020-10-04 10:24:57 +0530

    ignore this

---
+++ SGDClassifier/play.py
@@ -0,0 +1,10 @@
+from sklearn import svm, datasets
+from sklearn.model_selection import GridSearchCV
+iris = datasets.load_iris()
+parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10]}
+svc = svm.SVC()
+clf = GridSearchCV(svc, parameters)
+clf.fit(iris.data, iris.target)
+
+
+s1=sorted(clf.cv_results_.keys())
