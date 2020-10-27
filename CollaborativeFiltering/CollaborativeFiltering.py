c237255307cd18c2dc79425625cf5d85b0c99be2
darwin_b <darwin.bollepalli@gmail.com>
2020-10-27 12:12:32 +0530

    WIP : Needs fixes for memory issues

--- CollaborativeFiltering/CollaborativeFiltering.py
+++ CollaborativeFiltering/CollaborativeFiltering.py
@@ -25,21 +25,24 @@ with open(train_ratings_path,'r') as reader:
 
         data_matrix[map_users[user_id]][map_titles[title]] = rating
 
+# del reader
+mean_rating = np.nanmean(data_matrix,axis=1)
+# data_matrix[np.isnan(data_matrix)]=0
 
-mean_rating = np.nanmean(data_matrix,axis=1)[:,np.newaxis]
-
-deviation = data_matrix - mean_rating
+deviation = data_matrix - mean_rating[:,np.newaxis]
 deviation[np.isnan(deviation)]=0
 
-numerator_correlation = deviation.dot(deviation.T)
-d_correlation = (deviation**2).sum(axis=1)[:,np.newaxis]
+# numerator_correlation = deviation.dot(deviation.T)
+# d_correlation = (deviation**2).sum(axis=1)[:,np.newaxis]
+#
+# denominator_correlation = d_correlation.dot(d_correlation.T)
 
-denominator_correlation = d_correlation.dot(d_correlation.T)
-
-weights = numerator_correlation/denominator_correlation
-
-
+weights = {}
+ratings={}
 predicted = {}
+# temp =(deviation[1]*deviation).sum(axis=1)
+# temp2 =(deviation[1]**2).sum()*((deviation**2).sum(axis=1))
+
 with open(test_ratings_path,'r') as reader:
 
     for line in reader:
@@ -47,5 +50,22 @@ with open(test_ratings_path,'r') as reader:
         mapped_user = map_users[user_id]
         mapped_title = map_titles[title]
 
-        predicted[user_id] = mean_rating[mapped_user] + weights(user_id)*(data_matrix[:,mapped_title] - mean_rating)
-        break
+        n_correlation = (deviation[mapped_user]*deviation).sum(axis=1)
+        d_correlation = ((deviation[mapped_user]**2).sum())*((deviation**2).sum(axis=1))
+
+        if user_id not in weights:
+            weights[user_id]=n_correlation/d_correlation
+
+        normalising_constant = 1/np.nansum(weights[user_id])
+        weighted_sum = np.nansum(weights[user_id]*(data_matrix[:,mapped_title] - mean_rating))
+        predicted[user_id] = mean_rating[mapped_user] + weighted_sum*normalising_constant
+
+        # break
+
+
+# error=[]
+# with open(test_ratings_path,'r') as reader:
+#
+#     for line in reader:
+#         title,user_id,rating = line.split(',')
+#         if(float(rating)-predicted[user_id])
