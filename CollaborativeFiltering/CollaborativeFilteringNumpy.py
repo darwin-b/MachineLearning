62af54c034af4183e0cd658743b1f39a327506a1
darwin_b <darwin.bollepalli@gmail.com>
2020-10-28 02:49:26 +0530

    Computing as large array

--- CollaborativeFiltering/CollaborativeFilteringNumpy.py
+++ CollaborativeFiltering/CollaborativeFilteringNumpy.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 
@@ -7,7 +6,7 @@ test_ratings_path = "./../Data/netflix/TestingRatings.txt"
 
 map_users={}
 map_titles={}
-data_matrix = np.empty((28978,1821),dtype=np.float32)
+data_matrix = np.empty((28978,1821))
 data_matrix[:] = np.nan
 
 with open(train_ratings_path,'r') as reader:
@@ -25,22 +24,24 @@ with open(train_ratings_path,'r') as reader:
 
         data_matrix[map_users[user_id]][map_titles[title]] = rating
 
-del reader
-mean_rating = np.nanmean(data_matrix,axis=1)
-data_matrix[np.isnan(data_matrix)]=0
 
-deviation = data_matrix - mean_rating[:,np.newaxis]
+mean_rating = np.nanmean(data_matrix,axis=1)[:,np.newaxis]
+
+deviation = data_matrix - mean_rating
+deviation[np.isnan(deviation)]=0
+
+numerator_correlation = deviation.dot(deviation.T)
+d_correlation = (deviation**2).sum(axis=1)[:,np.newaxis]
+
+denominator_correlation = d_correlation.dot(d_correlation.T)
+
+weights = numerator_correlation/np.sqrt(denominator_correlation)
 
 
-weights = {}
-ratings={}
-predicted = {}
-
-squared_dev = (deviation**2).sum(axis=1)
 act_ratings=[]
 pred_ratings=[]
 error_rating=[]
-
+predicted = {}
 with open(test_ratings_path,'r') as reader:
     c=0
     for line in reader:
@@ -48,23 +49,10 @@ with open(test_ratings_path,'r') as reader:
         mapped_user = map_users[user_id]
         mapped_title = map_titles[title]
 
-
-        if user_id not in weights:
-            n_correlation = np.abs((deviation[mapped_user] * deviation).sum(axis=1))
-            d_correlation = np.sqrt(squared_dev[mapped_user] * squared_dev)
-            weights[user_id]=n_correlation/d_correlation
-
-        normalising_constant = weights[user_id].sum()
-        weighted_sum = (weights[user_id]*(data_matrix[:,mapped_title] - mean_rating)).sum()
-        predicted[(mapped_title,user_id)] = mean_rating[mapped_user] + weighted_sum/normalising_constant
+        predicted[user_id] = mean_rating[mapped_user] + weights[mapped_user]*(data_matrix[:,mapped_title] - mean_rating)
         act_ratings.append(float(rating.replace("\n", "")))
         error_rating.append(float(rating.replace("\n", ""))-predicted[(mapped_title,user_id)])
 
         print(c," Acct : ",float(rating.replace("\n", "")), "Pred : ",predicted[(mapped_title,user_id)])
         c+=1
-
-
-temp = np.arange(6)
-temp = temp/normalising_constant
-
-temp = np.cov(deviation)
+        # break
