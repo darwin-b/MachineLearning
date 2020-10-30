2ef84b8789e08c0f02023ab4ded93f863e12835a
darwin_b <darwin.bollepalli@gmail.com>
2020-10-30 18:37:22 +0530

    Operations on Ndarray (large memory for computation will be allocated if available)

--- CollaborativeFiltering/CollaborativeFilteringNumpy.py
+++ CollaborativeFiltering/CollaborativeFilteringNumpy.py
@@ -9,6 +9,9 @@ map_titles={}
 data_matrix = np.empty((28978,1821))
 data_matrix[:] = np.nan
 
+'''
+Reading Train Ratings
+'''
 with open(train_ratings_path,'r') as reader:
     counter_titles=0
     counter_users = 0
@@ -24,20 +27,28 @@ with open(train_ratings_path,'r') as reader:
 
         data_matrix[map_users[user_id]][map_titles[title]] = rating
 
-
+'''
+Calculating Mean of voted ratings for each user
+'''
 mean_rating = np.nanmean(data_matrix,axis=1)
 
 deviation = data_matrix - mean_rating[:,np.newaxis]
+
+'''
+Replacing Nan values with Zeroes
+'''
 data_matrix[np.isnan(data_matrix)]=0
 deviation[np.isnan(deviation)]=0
 
 numerator_correlation = deviation.dot(deviation.T)
 d_correlation = (deviation**2).sum(axis=1)[:,np.newaxis]
-
 denominator_correlation = d_correlation.dot(d_correlation.T)
 
 weights = numerator_correlation/np.sqrt(denominator_correlation)
 
+'''
+Replacing any Nan or invalid values due to divison of 0 with Zeros
+'''
 weights[np.isnan(weights)]=0
 weights[np.isinf(weights)]=0
 
@@ -46,6 +57,12 @@ act_ratings=[]
 pred_ratings=[]
 error_rating=[]
 predicted = {}
+MAE =0
+abs_error=0
+squared_error=0
+'''
+Predicting Train Ratings & computing MSE & MAE
+'''
 with open(test_ratings_path,'r') as reader:
     c=0
     for line in reader:
@@ -56,22 +73,22 @@ with open(test_ratings_path,'r') as reader:
         normalising_constant = weights[mapped_user].sum()
         predicted[(mapped_title,user_id)] = mean_rating[mapped_user] + (weights[mapped_user].dot(data_matrix[:,mapped_title] - mean_rating))/normalising_constant
         act_ratings.append(float(rating.replace("\n", "")))
-        error_rating.append(float(rating.replace("\n", ""))-predicted[(mapped_title,user_id)])
-
+        error =  float(rating.replace("\n", ""))-predicted[(mapped_title,user_id)]
+        error_rating.append(error)
+        abs_error += abs(error)
+        squared_error += error**2
         print(c," Acct : ",float(rating.replace("\n", "")), "Pred : ",predicted[(mapped_title,user_id)])
         c+=1
         # break
 
-# avg_absMean =
+
+print("\n\n","Mean absolute Error : ",abs_error/len(act_ratings))
+print("Mean Squared Error : ",squared_error/len(act_ratings))
+
+print("\nErrors (MAE, MSE)",abs_error,squared_error)
 
 file_name="resultsMatrix_NetflixPredictions"+".txt"
 with open(file_name,'w') as file:
-    # text = "ham is ham : "+str(acc_ham)+"\n"+"spam is spam : "+str(acc_spam)+"\n"+"\n Total accuracy : "+str(acc_total)+"\n"
-    # text = text + " precision : " + str(precision) + "\n"
-    # text = text + " Recall : " + str(recall) + "\n"
-    # text = text + " F1_score : " + str(f1_score) + "\n\n"
-    # text = text+" mislabel ham file indices : "+repr(mislabel_ham)+"\n"+" mislabel spam file indices : "+repr(mislabel_spam)+"\n\n"
-    # text = text +"Bag of Ham :\n"+ repr(bag_ham)+"\n\n"+"Bag of Spam :\n"+ repr(bag_spam)
-
-
-    # file.write(text)
+    text = "MAE : "+str(abs_error/len(act_ratings))+"\n"+"MSE : "+str(squared_error/len(act_ratings))+"\n\n"
+    text = text + "-----------------------------------------------------------------------------------------\n"
+    text = text + " Predicted Ratings : \n" + str(predicted)
