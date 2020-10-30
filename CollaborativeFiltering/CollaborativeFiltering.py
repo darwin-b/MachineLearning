ba1950078be3e04fa725aa23a84fb2bd19970b67
darwin_b <darwin.bollepalli@gmail.com>
2020-10-30 18:35:07 +0530

    Due to memorry issues: spliting operations on Numpy Array

--- CollaborativeFiltering/CollaborativeFiltering.py
+++ CollaborativeFiltering/CollaborativeFiltering.py
@@ -7,7 +7,7 @@ test_ratings_path = "./../Data/netflix/TestingRatings.txt"
 
 map_users={}
 map_titles={}
-data_matrix = np.empty((28978,1821))
+data_matrix = np.empty((28978,1821),dtype=np.float32)
 data_matrix[:] = np.nan
 
 with open(train_ratings_path,'r') as reader:
@@ -25,47 +25,42 @@ with open(train_ratings_path,'r') as reader:
 
         data_matrix[map_users[user_id]][map_titles[title]] = rating
 
-# del reader
+del reader
 mean_rating = np.nanmean(data_matrix,axis=1)
-# data_matrix[np.isnan(data_matrix)]=0
+data_matrix[np.isnan(data_matrix)]=0
 
 deviation = data_matrix - mean_rating[:,np.newaxis]
-deviation[np.isnan(deviation)]=0
 
-# numerator_correlation = deviation.dot(deviation.T)
-# d_correlation = (deviation**2).sum(axis=1)[:,np.newaxis]
-#
-# denominator_correlation = d_correlation.dot(d_correlation.T)
 
 weights = {}
 ratings={}
 predicted = {}
-# temp =(deviation[1]*deviation).sum(axis=1)
-# temp2 =(deviation[1]**2).sum()*((deviation**2).sum(axis=1))
+
+squared_dev = (deviation**2).sum(axis=1)
+act_ratings=[]
+pred_ratings=[]
+error_rating=[]
 
 with open(test_ratings_path,'r') as reader:
-
+    c=0
     for line in reader:
         title,user_id,rating = line.split(',')
         mapped_user = map_users[user_id]
         mapped_title = map_titles[title]
 
-        n_correlation = (deviation[mapped_user]*deviation).sum(axis=1)
-        d_correlation = ((deviation[mapped_user]**2).sum())*((deviation**2).sum(axis=1))
 
         if user_id not in weights:
+            n_correlation = np.abs((deviation[mapped_user] * deviation).sum(axis=1))
+            d_correlation = np.sqrt(squared_dev[mapped_user] * squared_dev)
             weights[user_id]=n_correlation/d_correlation
 
-        normalising_constant = 1/np.nansum(weights[user_id])
-        weighted_sum = np.nansum(weights[user_id]*(data_matrix[:,mapped_title] - mean_rating))
-        predicted[user_id] = mean_rating[mapped_user] + weighted_sum*normalising_constant
+        normalising_constant = weights[user_id].sum()
+        weighted_sum = (weights[user_id]*(data_matrix[:,mapped_title] - mean_rating)).sum()
+        predicted[(mapped_title,user_id)] = mean_rating[mapped_user] + weighted_sum/normalising_constant
+        act_ratings.append(float(rating.replace("\n", "")))
+        error_rating.append(float(rating.replace("\n", ""))-predicted[(mapped_title,user_id)])
 
-        # break
+        print(c," Acct : ",float(rating.replace("\n", "")), "Pred : ",predicted[(mapped_title,user_id)])
+        c+=1
 
 
-# error=[]
-# with open(test_ratings_path,'r') as reader:
-#
-#     for line in reader:
-#         title,user_id,rating = line.split(',')
-#         if(float(rating)-predicted[user_id])
